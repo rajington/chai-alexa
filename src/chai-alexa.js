874348631337742d44eb2577933cdb3a9014e2ad
Rajarshi Nigam <raj@nigam.com>
2016-08-16 08:20:29 -0700

    basic checking for phrase in Alexa responses

--- src/chai-alexa.js
+++ src/chai-alexa.js
@@ -1,7 +1,50 @@
-const chaiAlexa = {
-  greet() {
-    return 'hello';
-  },
-};
+/* eslint no-underscore-dangle: ["error", { "allowAfterThis": true }]*/
+// import chaiJsonSchema from 'chai-json-schema';
+// import { skillsKitResponse } from 'alexa-schemas';
+import { get } from 'lodash';
 
-export default chaiAlexa;
+export default function (chai, utils) {
+  // expect(response).to.have.sessionAttributes
+  // chai.use(chaiJsonSchema);
+  // chai.tv4.banUnknown = true;
+  // chai.tv4.addSchema(skillsKitResponse);
+
+  chai.Assertion.overwriteMethod('phrase', () =>
+    function assertPhrase(phrase) {
+      const obj = utils.flag(this, 'object');
+
+      // // validate response
+      // new Assertion(this._obj).to.be.jsonSchema(skillsKitResponse);
+
+      const getResponse = property => {
+        const output = get(obj, `response.${property}`);
+        if (output && property.includes('ssml')) {
+          return output.replace(/<\/?[^>]+>/g, ''); // removes all SSML tags
+        }
+        return output;
+      };
+
+      // combine all the possible locations
+      const output = [
+        'outputSpeech.text',
+        'outputSpeech.ssml',
+        'reprompt.outputSpeech.text',
+        'reprompt.outputSpeech.ssml',
+        'card.title',
+        'card.content',
+        'card.text',
+      ].map(getResponse)
+      .filter(response => response) // filter out nulls
+      .join();
+
+      // TODO: better messages, e.g. where it was found
+      this.assert(
+          output.includes(phrase)
+        , 'expected #{this} to have phrase "#{exp}" but got #{act}'
+        , 'expected #{this} to not have phrase "#{exp}" in #{act}'
+        , phrase   // expected
+        , output   // actual
+      );
+    }
+  );
+}
--- test/unit/chai-alexa.js
+++ test/unit/chai-alexa.js
@@ -1,18 +1,49 @@
+import chai from 'chai';
 import chaiAlexa from '../../src/chai-alexa';
 
+chai.use(chaiAlexa);
+
 describe('chaiAlexa', () => {
-  describe('Greet function', () => {
-    beforeEach(() => {
-      spy(chaiAlexa, 'greet');
-      chaiAlexa.greet();
+  describe('assertPhrase', () => {
+    const response = {
+      version: '1.0',
+      sessionAttributes: {
+        key: 'this is in session attributes text',
+      },
+      response: {
+        outputSpeech: {
+          type: 'PlainText',
+          text: 'this is in plain text',
+        },
+        card: {
+          type: 'Standard',
+          title: 'this is in title text',
+          text: 'this is in card text',
+        },
+        reprompt: {
+          outputSpeech: {
+            type: 'SSML',
+            ssml: '<speak>this is in <s>ssml</s> <p>text</p></speak>',
+          },
+        },
+        shouldEndSession: false,
+      },
+    };
+
+    it('should find phrase in text', () => {
+      expect(response).to.have.phrase('plain text');
     });
 
-    it('should have been run once', () => {
-      expect(chaiAlexa.greet).to.have.been.calledOnce;
+    it('should not find phrase in sessionAttributes', () => {
+      expect(response).to.not.have.phrase('session attributes text');
     });
 
-    it('should have always returned hello', () => {
-      expect(chaiAlexa.greet).to.have.always.returned('hello');
+    it('should find phrase in ssml text', () => {
+      expect(response).to.have.phrase('ssml text');
+    });
+
+    it('should find phrase in card text', () => {
+      expect(response).to.have.phrase('card text');
     });
   });
 });
